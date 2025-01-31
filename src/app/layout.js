22dbbe2dbb67dfc5cf7fac83792bf14806516ac5
Ethan Lee <ethan.lee@emory.edu>
2025-01-31 15:49:36 -0500

    Update site title

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Hanken_Grotesk } from "next/font/google";
 const hankenGrotesk = Hanken_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "FMs4Bio",
+  title: "FMs4Bio - AAAI 2025",
   description:
     "2nd AAAI Workshop on Foundation Models for Biological Discoveries (FMs4Bio)",
 };
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${hankenGrotesk.className} antialiased`}
-      >
+      <body className={`${hankenGrotesk.className} antialiased`}>
         <main>{children}</main>
       </body>
     </html>
